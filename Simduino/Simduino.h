27a50759478318a7a62fa28cec398881459e5d7a
Carl Peto <carl.peto@me.com>
2020-01-31 17:14:28 +0000

    initial commit, bare bones xcode project

---
+++ Simduino/Simduino.h
@@ -0,0 +1,14 @@
+//
+//  Simduino.h
+//  Simduino
+//
+//  Created by Carl Peto on 31/01/2020.
+//  Copyright © 2020 Carl Peto. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "SimduinoProtocol.h"
+
+// This object implements the protocol which we have defined. It provides the actual behavior for the service. It is 'exported' by the service to make it available to the process hosting the service over an NSXPCConnection.
+@interface Simduino : NSObject <SimduinoProtocol>
+@end
---
+++ Simduino/Simduino.m
@@ -0,0 +1,19 @@
+//
+//  Simduino.m
+//  Simduino
+//
+//  Created by Carl Peto on 31/01/2020.
+//  Copyright © 2020 Carl Peto. All rights reserved.
+//
+
+#import "Simduino.h"
+
+@implementation Simduino
+
+// This implements the example protocol. Replace the body of this class with the implementation of this service's protocol.
+- (void)upperCaseString:(NSString *)aString withReply:(void (^)(NSString *))reply {
+    NSString *response = [aString uppercaseString];
+    reply(response);
+}
+
+@end
